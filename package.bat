5c61e7d998c6c6344b9f2a06f58d4e305d890322
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-06 15:11:12 -0700

    Example packaging script

---
+++ package.bat
@@ -0,0 +1,28 @@
+:: Create a mods\ folder to add to .zip
+mkdir mods
+
+:: Go into folder with scripts
+cd data\scripts
+
+:: Add our global script(s) to a .dat file
+dat2 a -t scripts Ouchie.dat gl_ouchie_ouch.int
+:: dat2 a -t scripts Ouchie.dat gl_second.int
+:: dat2 a -t scripts Ouchie.dat gl_third.int
+
+:: Move created .dat into mods\ folder
+move Ouchie.dat ..\..\mods
+
+:: Go to the root folder
+cd ..\..
+
+:: Copy .ini into mods\ folder
+copy Ouchie.ini mods\
+
+:: Add mods\ folder to .zip
+7z a Ouchie.zip mods\
+
+:: Wait a moment before deleting otherwise 7z might still be archiving...
+timeout /t 1
+
+:: Delete the mods\ folder
+rmdir /S /Q mods
